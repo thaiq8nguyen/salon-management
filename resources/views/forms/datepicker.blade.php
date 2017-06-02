afb273070a2aebf271e80817668211c1c42a0356
Thai Nguyen <discoverylight@yahoo.com>
2017-06-01 20:45:05 -0700

    -Update Salon daily sales function
    - Update SalonSaleApi controller with remove Validator class and use the default validate helper method
    - Update SalonSale css by adding a panel style
    - Remove moment js and vue js from the Sale-Entry.js to reduce file size
    - Add Error class for form validation error, add onSuccess and onError callback functions
    - Create a reuseable datepicker input

---
+++ resources/views/forms/datepicker.blade.php
@@ -0,0 +1,6 @@
+<div class = "form-group">
+    <div class = "input-group">
+        <div class = "input-group-addon"><i class = "fa fa-calendar"></i></div>
+        <datepicker v-model="date" :format="dateFormat" :bootstrap-styling="bootstrapStyling"></datepicker>
+    </div>
+</div>
