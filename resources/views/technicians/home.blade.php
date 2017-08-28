320cd7add172903419820be62db82de5148431f9
Thai Nguyen <discoverylight@yahoo.com>
2017-08-27 22:48:51 -0700

    fix sticky footer

--- resources/views/technicians/home.blade.php
+++ resources/views/technicians/home.blade.php
@@ -3,14 +3,13 @@
 @push('styles')
     <link rel = "stylesheet" href = "{{ asset('css/vuetify.min.css') }}">
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet" type="text/css">
+
 @endpush
 @push('scripts')
     <script src = "{{ asset('js/technicians/technician.js')}}"></script>
 @endpush
 @section('content')
     <div class = 'main-content' id = "root">
-
         <technician></technician>
-
     </div>
 @endsection
