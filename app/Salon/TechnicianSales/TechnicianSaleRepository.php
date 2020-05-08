c5080f43cadc26d69db2d4dc897fa95f7f778e42
Thai Nguyen <discoverylight@yahoo.com>
2020-05-08 10:24:41 -0700

    started working on a running balance after a transaction is added

--- app/Salon/TechnicianSales/TechnicianSaleRepository.php
+++ app/Salon/TechnicianSales/TechnicianSaleRepository.php
@@ -16,34 +16,43 @@ class TechnicianSaleRepository implements TechnicianSaleInterface
     public function addTechnicianSale($sale)
     {
         // find the technician account using the technician Id
-
         $user = User::find($sale['technician_id']);
 
         // find the technician role user Id
         $technicianRole = Role::where('name', 'technician')->first();
         $technicianRoleUserId = $user->roles()->wherePivot('role_id', $technicianRole->id)->first();
 
-        // find the technician account
-
+        // find the technician book account in the accounts table
         $technicianAccount = Account::where('role_user_id', $technicianRoleUserId->pivot->id)->first();
 
         // insert technician sale and technician tip (if any) into as transactions under the technician account
+        $saleTransaction = '';
         if ($sale['sales']) {
             $saleItem = TransactionItem::item('technician sales')->first();
             $saleTransaction = Transaction::create([
                 'account_id' => $technicianAccount->id,
                 'transaction_item_id' => $saleItem->id,
                 'date' => $sale['date'],
-                'note' =>$sale['note'],
+                'note' => $sale['note'] ? $sale['note'] : '',
                 'credit' => $sale['sales']
             ]);
         }
+        $tipTransaction = '';
+        if ($sale['tips']) {
+            $tipItem = TransactionItem::item('technician tips')->first();
+            $tipTransaction = Transaction::create([
+                'account_id' => $technicianAccount->id,
+                'transaction_item_id' => $tipItem->id,
+                'date' => $sale['date'],
+                'note' => $sale['note'] ? $sale['note'] : '',
+                'credit' => $sale['tips']
+            ]);
+        }
 
+        $created = ['sale' => Transaction::where('id',$saleTransaction->id)->first(['credit'])];
+        //$created = $saleTransaction->id;
+        return $created;
 
-        return true;
-
-
-        //$sale = Transaction::create(['owner_id' => $technicianId, '']);
     }
 
     public function updateTechnicianSale($saleId, $sale)
